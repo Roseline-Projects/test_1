9cf129cfcac5814c70cc98d1bf15ed6a7a62e080
Roseline Damiscar <rdami007@fiu.edu>
2023-09-08 22:27:26 -0400

    testing code

---
+++ javacodetest.java
@@ -0,0 +1,14 @@
+public class testJava{
+
+	public static void main{String[] args) {
+
+		int sky = 1;
+		int ground = 2;
+
+		int world = sky + ground;
+
+		System.out.println("Wow it's the " + world);
+
+	}//end main
+
+}//end class
